950683e0236a1c812f21d07445bb6658d7812f39
Marian Dandarila <marian_dandarila@yahoo.com>
2020-12-02 19:47:44 +0200

    updating stripe integration and updating our favicon and title

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { createStructeredSelector } from 'reselect';
-
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import CheckoutPage from './pages/checkout/checkout.component';
-import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/signin-in-and-sign-up/signin-in-and-sign-up.component';
-import { auth, createUserProfileDocument} from './firebase/firebase.utils';
-import { setCurrentUser } from './redux/user/user.actions';
-import { selectCurrentUser } from './redux/user/user.selectors';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/signin-in-and-sign-up/signin-in-and-sign-up.component.jsx'
+import CheckoutPage from './pages/checkout/checkout.component';
 
+import Header from './components/header/header.component';
+
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
-  unsubscribeFromAuth = null
-
+  unsubscribeFromAuth = null;
 
   componentDidMount() {
-
     const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth) { 
+      if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
           setCurrentUser({
-            id: snapShot.id, 
+            id: snapShot.id,
             ...snapShot.data()
           });
         });
-        
-      } else {
-        setCurrentUser(userAuth);
       }
-    });  
+
+      setCurrentUser(userAuth);
+    });
   }
 
   componentWillUnmount() {
@@ -49,19 +48,19 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
-          <Route exact path='/' component ={HomePage} />
-          <Route path='/shop' component ={ShopPage} />
-          <Route exact path='/checkout' component ={CheckoutPage} />
-          <Route 
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
+          <Route
             exact
-            path='/signin' 
-            render={() => 
+            path='/signin'
+            render={() =>
               this.props.currentUser ? (
                 <Redirect to='/' />
               ) : (
-                <SignInAndSignUpPage /> 
+                <SignInAndSignUpPage />
               )
-            } 
+            }
           />
         </Switch>
       </div>
@@ -69,14 +68,15 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
+const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
-
-
-const mapDispatchToProps = dispatch =>({
+const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,7 @@ import  { createStructuredSelector } from 'reselect';
 import CheckoutItem  from '../../components/checkout-item/checkout-item.component'
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 
 import './checkout.styles.scss';
@@ -32,9 +33,14 @@ import './checkout.styles.scss';
         ))
       }
 
-      <div className='total'>
-    <span> TOTAL: ${total}</span>
+      <div className='total'> TOTAL: ${total}</div>
+      <div className='test-warning'>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/21 - CVV: 123
+
       </div>
+      <StripeCheckoutButton price={total} />
     </div>
 );
 
