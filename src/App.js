a5ce952c3e0532ddcfc9ce36f8658b4dfcb8347e
Marian Dandarila <marian_dandarila@yahoo.com>
2020-12-07 16:22:03 +0200

    implement code that pulss in our shop data from firestore and stores it into the shope reducer from our shop page. Also included our WithSpinner HOC

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
@@ -17,10 +19,15 @@ import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
 
+
+
+
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+
+
     const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
@@ -36,6 +43,7 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
+      
     });
   }
 
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -9,7 +9,7 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections], 
-  collections => Object.keys(collections).map(key => collections[key])
+  collections => collections ? Object.keys(collections).map(key => collections[key]) : []
 )
 
 
@@ -17,5 +17,5 @@ export const selectCollectionsForPreview = createSelector(
 export const selectCollection = collectionUrlParam => 
   createSelector(
     [selectCollections], 
-    collections => collections[collectionUrlParam]
+    collections => (collections ? collections[collectionUrlParam] : null)
   );
